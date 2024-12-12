451d797ce94ebfdff55f59efa98e865cf73f0b99
Konstantin Lazarev <konstantinlazarev@Konstantins-MacBook-Pro.local>
2024-12-12 15:28:36 +0300

    Add index on cars.price

---
+++ db/migrate/20241212115821_add_indices_to_cars.rb
@@ -0,0 +1,5 @@
+class AddIndicesToCars < ActiveRecord::Migration[6.1]
+  def change
+    add_index :cars, :price
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_121316) do
+ActiveRecord::Schema.define(version: 2024_12_12_115821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_17_121316) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["brand_id"], name: "index_cars_on_brand_id"
+    t.index ["price"], name: "index_cars_on_price"
   end
 
   create_table "user_preferred_brands", force: :cascade do |t|
